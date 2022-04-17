c85bc5c5f4c658ff6a8a29936b61978f6d46e5cd
Safinxr <safinxr@gmail.com>
2022-04-17 06:02:22 +0600

    pages added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Pages/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Services from './Pages/Services/Services';
+import Blog from './Pages/Blog/Blog';
+import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
+import Signup from './Pages/Signup/Signup';
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/services' element={<Services></Services>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/About' element={<About></About>}></Route>
+        <Route path='/Login' element={<Login></Login>}></Route>
+        <Route path='/Signup' element={<Signup></Signup>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
