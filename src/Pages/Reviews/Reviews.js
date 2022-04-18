461fd3e497cd9086e36d942f09fee09bf3882908
Safinxr <safinxr@gmail.com>
2022-04-18 06:19:26 +0600

    review area added

--- src/Pages/Reviews/Reviews.js
+++ src/Pages/Reviews/Reviews.js
@@ -1,11 +1,75 @@
-import React from 'react';
+import React from "react";
+import "./Reviews.css";
 
 const Reviews = () => {
-    return (
-        <div>
-            <h1>lasdfjkladjf</h1>
+  return (
+    <div className="review-bg my-5 py-5">
+      <h1 className="text-center text-white fw-bold mb-4">People's Review</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-6 col-lg-4 d-flex flex-column align-items-center">
+            <div className="bg-white p-5 radius">
+              <p>
+                We all had to start somewhere, but i can tell for sure that Fit
+                Boss knows exactly how to effectively learn about healthy
+                lifestyle and working out. He will be there for you with his
+                professional program and natritional diet plan. The results are
+                amazing
+              </p>
+            </div>
+            <div className="d-flex flex-column align-items-center position">
+              <img
+              width={70}
+                className="rounded-circle"
+                src="https://niobe.axiomthemes.com/wp-content/uploads/2016/10/1test-98x98.jpg"
+                alt=""
+              />
+              <p className="card-text text-white text center fw-bold mb-0">Revecca Stevens</p>
+              <p className="card-text text-white text center ">Tennis Player</p>
+              
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-4 d-flex flex-column align-items-center">
+            <div className="bg-color p-5 radius">
+              <p className="text-white">
+                I love your hard work and the devotion to every trainee. The results were almost immediate really, if you know that your trainer is a professional you can become a completely different self within months. Think positive eat good and start working out with Fit boss.
+              </p>
+            </div>
+            <div className="d-flex flex-column align-items-center position">
+              <img
+              width={70}
+                className="rounded-circle"
+                src="https://niobe.axiomthemes.com/wp-content/uploads/2016/09/testim-2-98x98.jpg"
+                alt=""
+              />
+              <p className="card-text text-white text center fw-bold mb-0">Arron Keanly</p>
+              <p className="card-text text-white text center ">Model</p>
+              
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-4 d-flex flex-column align-items-center">
+            <div className="bg-white p-5 radius">
+              <p>
+              Fit Boss is an awesome trainer to work with: he is a positive and strong man with kind and sincere intentions to help you! If you are a beginner, he will teach you everything and will be supporting you throughout the entire journey to the body of you dream.
+              </p>
+            </div>
+            <div className="d-flex flex-column align-items-center position">
+              <img
+              width={70}
+                className="rounded-circle"
+                src="https://niobe.axiomthemes.com/wp-content/uploads/2016/10/3test-98x98.jpg"
+                alt=""
+              />
+              <p className="card-text text-white text center fw-bold mb-0">Debbie Franklin</p>
+              <p className="card-text text-white text center ">Designer</p>
+              
+            </div>
+          </div>
+          
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Reviews;
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,59 +1,62 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const servicesData = [
-    {
-        id: 1,
-        img: 'https://img.freepik.com/free-photo/couple-gym_1303-5515.jpg?t=st=1650195226~exp=1650195826~hmac=4bee31a147ccedefa8bde0356b67dfaa707c0d1993b1fcbabaa5aa0e13d95758&w=740',
-        title: 'WEIGHT LOSS PROGRAM',
-        dc: "I don't just show you how to do an exercise; I educate you in the areas of strength training, cardiovascular endurance, and proper nutrition."
-    },
-    {
-        id: 2,
-        img: 'https://img.freepik.com/free-photo/woman-doing-workout-gym-with-trainer_1303-25221.jpg?t=st=1650195226~exp=1650195826~hmac=c754f917994bc2ff6a820aa3a2061a72bb7663c1f98d07fd7cf3cf81d61a52c9&w=740',
-        title: 'HEAVY WORKOUT PROGRAM',
-        dc: 'Understanding the way your body and its processes work and how to influence it is the most important step to your dream body'
-    },
-    {
-        id: 3,
-        img: 'https://img.freepik.com/free-photo/woman-gym-with-coach_1303-5545.jpg?w=740',
-        title: 'PREMIUM WORKOUT PROGRAM',
-        dc: 'A professionally developed personal training program that was created specifically for you will take you as far as you want to go.'
-    }
-
-]
+  {
+    id: 1,
+    price: 44,
+    img: "https://img.freepik.com/free-photo/couple-gym_1303-5515.jpg?t=st=1650195226~exp=1650195826~hmac=4bee31a147ccedefa8bde0356b67dfaa707c0d1993b1fcbabaa5aa0e13d95758&w=740",
+    title: "WEIGHT LOSS PROGRAM",
+    dc: "I don't just show you how to do an exercise; I educate you in the areas of strength training, cardiovascular endurance, and proper nutrition.",
+  },
+  {
+    id: 2,
+    price: 55,
+    img: "https://img.freepik.com/free-photo/woman-doing-workout-gym-with-trainer_1303-25221.jpg?t=st=1650195226~exp=1650195826~hmac=c754f917994bc2ff6a820aa3a2061a72bb7663c1f98d07fd7cf3cf81d61a52c9&w=740",
+    title: "HEAVY WORKOUT PROGRAM",
+    dc: "Understanding the way your body and its processes work and how to influence it is the most important step to your dream body",
+  },
+  {
+    id: 3,
+    price: 66,
+    img: "https://img.freepik.com/free-photo/woman-gym-with-coach_1303-5545.jpg?w=740",
+    title: "PREMIUM WORKOUT PROGRAM",
+    dc: "A professionally developed personal training program that was created specifically for you will take you as far as you want to go.",
+  },
+];
 
 const Services = () => {
-    return (
-        <div className='my-5 container'>
-            <h1 className='text-center text-info fw-bold mb-4'>Services</h1>
+  return (
+    <div className="my-5 container">
+      <h1 className="text-center text-info fw-bold mb-4">Services</h1>
 
-            <div className="row row-cols-1 row-cols-md-3 g-5    ">
-                {
-                    servicesData.map(data => <Service key={data.id} cardInfo={data} ></Service>)
-                }
-            </div>
-        </div>
-    );
+      <div className="row row-cols-1 row-cols-md-3 g-5">
+        {servicesData.map((data) => (
+          <Service key={data.id} cardInfo={data}></Service>
+        ))}
+      </div>
+    </div>
+  );
 };
 const Service = ({ cardInfo }) => {
-    const { img, title, dc } = cardInfo
-    return (
-        <div className="col ">
-            <div className="card h-100 shadow-lg">
-                <img src={img} className="card-img-top" alt="..." />
-                <div className="card-body pb-0">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{dc}</p>
-                </div>
-                <div class="card-footer border-0 mx-auto pb-3">
-                    <Link className='btn bg-color px-4 text-white' to='/checkout'>Check out </Link>
-                </div>
-
-            </div>
+  const { img, title, dc, price } = cardInfo;
+  return (
+    <div className="col ">
+      <div className="card h-100 shadow-lg">
+        <img src={img} className="card-img-top" alt="..." />
+        <div className="card-body pb-0">
+          <h5 className="card-title text-info">{title}</h5>
+          <p className="card-text">{dc}</p>
+          <p className="card-text h5">Price: ${price}</p>
         </div>
-    )
-}
-
+        <div class="card-footer border-0 mx-auto pb-3">
+          <Link className="btn bg-color px-4 text-white" to="/checkout">
+            Check out{" "}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Services;
